2fc504003edec0631ca6eedc06cd115c2089399c
alfredo brizuela <alfredobrizuela1@gmail.com>
2022-01-31 19:04:12 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import app from './app';
+import './database';
+import { PORT } from './config'
+
+//------------------------------------------------------------------------------//
+
+app.listen(PORT);
+console.log("Server on port", PORT);
